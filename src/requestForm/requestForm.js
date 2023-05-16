815e3fefff1f6f11ac2104e17a9abc533ec460ed
benzinow <benzinow@gmail.com>
2023-05-15 17:14:02 -0700

    added favicon

---
+++ src/requestForm/requestForm.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Footer from "./Footer";
+import "./App.css";
+
+function requestForm() {
+  return (
+    <div className="request-form">
+      <div className="body">
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default requestForm;
