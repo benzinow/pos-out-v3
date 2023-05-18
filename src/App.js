c311434358d4b6e530ddd9afb9aaec31ba99925c
benzinow <benzinow@gmail.com>
2023-05-18 15:10:11 -0700

    removeed loader for now

--- src/App.js
+++ src/App.js
@@ -1,40 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Header from "./Header";
 import Fixables from "./Fixables";
 import Bio from "./Bio";
 import Testimonies from "./Testimonies";
 import Footer from "./Footer";
-import Loader from "./Loader";
 import "./App.css";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const handleLoad = () => {
-      setLoading(false);
-    };
-
-    window.addEventListener("load", handleLoad);
-
-    return () => {
-      window.removeEventListener("load", handleLoad);
-    };
-  }, []);
-
   return (
     <div className="App">
-      {loading ? (
-        <Loader />
-      ) : (
-        <div className="body">
-          <Header />
-          <Fixables />
-          <Bio />
-          <Testimonies />
-          <Footer />
-        </div>
-      )}
+      <div className="body">
+        <Header />
+        <Fixables />
+        <Bio />
+        <Testimonies />
+        <Footer />
+      </div>
     </div>
   );
 }
