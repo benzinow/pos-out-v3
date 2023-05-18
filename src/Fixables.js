0a2a11b2e290ad7953799a808d6fae730d46846c
benzinow <benzinow@gmail.com>
2023-05-18 14:37:38 -0700

    dynamic text and images in the fixabled section of the website

--- src/Fixables.js
+++ src/Fixables.js
@@ -1,11 +1,30 @@
 import React, { useState } from "react";
 import Request from "./Request";
 
+// Import the images
+import smokingImage from "./images/smoking.svg";
+import stressImage from "./images/stress.svg";
+import weightImage from "./images/weight.svg";
+import painImage from "./images/pain.svg";
+import fearImage from "./images/fear.svg";
+
 function Fixables() {
   const [selectedTopic, setSelectedTopic] = useState("Quit Smoking");
 
   const handleTopicClick = (topic) => {
     setSelectedTopic(topic);
+  };
+
+  const getImageSource = (topic) => {
+    const imageSources = {
+      "Quit Smoking": smokingImage,
+      "Reduce Stress": stressImage,
+      "Lose Weight": weightImage,
+      "Manage Pain": painImage,
+      "Overcome Fears": fearImage,
+    };
+
+    return imageSources[topic];
   };
 
   return (
@@ -53,36 +72,80 @@ function Fixables() {
         </div>
       </div>
       <div className="fix-facts">
-        <div className="fix-facts-icon"></div>
+        <img
+          className="fix-facts-icon"
+          src={getImageSource(selectedTopic)}
+          alt="Topic Icon"
+        />
         <div className="fix-facts-text">
           {selectedTopic === "Reduce Stress" && (
-            <p>Information specific to the "Reduce Stress" topic goes here.</p>
+            <p>
+              Stress can significantly impact our daily lives, affecting our
+              well-being and overall quality of life. Through hypnotherapy, you
+              can find effective ways to reduce anxiety and regain control.
+              During a session, you will experience deep relaxation and
+              heightened receptiveness to positive suggestion. Together, we will
+              work on reprogramming your subconscious mind, enabling you to
+              perceive triggers differently and develop healthier coping
+              mechanisms. Let's uncover the underlying factors contributing to
+              your stress and pave the way for a calmer and more balanced life.
+            </p>
           )}
           {selectedTopic === "Lose Weight" && (
-            <p>Information specific to the "Lose Weight" topic goes here.</p>
+            <p>
+              Losing weight can be a challenging journey, but with the help of
+              hypnotherapy, you can achieve lasting results. During our
+              sessions, you will enter a state of deep relaxation, where your
+              subconscious mind becomes open to positive suggestions and new
+              perspectives. Together, we will address the root causes of
+              overeating or unhealthy habits, establish a healthy relationship
+              with food, and develop motivation and willpower to achieve your
+              weight loss goals. Let's embark on this transformative journey
+              towards a healthier, fitter, and more confident you.
+            </p>
           )}
           {selectedTopic === "Quit Smoking" && (
             <p>
-              Hypnotherapy has been shown to be an effective tool for helping
-              individuals quit smoking. During a session, you will be guided
-              into a state of deep relaxation becoming more open to suggestion.
-              We then work to reprogram your subconscious mind, helping you to
-              view smoking in a negative light and promoting healthier habits
-              and coping mechanisms. I will help you identify possible
-              underlying emotional or psychological issues that are contributing
-              to smoking habits. Hypnotherapy can create powerful insight
-              towards your journey to a nicotine-free life!
+              Quitting smoking is a life-changing decision that can greatly
+              improve your health and well-being. Through hypnotherapy, you can
+              tap into the power of your subconscious mind to break free from
+              the smoking habit. Our sessions will focus on reprogramming
+              deep-seated beliefs and associations related to smoking,
+              strengthening your willpower, and developing healthier coping
+              strategies. Together, we will work towards your goal of becoming a
+              non-smoker, empowering you to live a smoke-free and healthier
+              life.
             </p>
           )}
           {selectedTopic === "Manage Pain" && (
-            <p>Information specific to the "Manage Pain" topic goes here.</p>
+            <p>
+              Chronic pain can be debilitating, affecting various aspects of
+              your life. Hypnotherapy offers a natural and effective approach to
+              pain management, helping you regain control and find relief. By
+              accessing your subconscious mind, we can address the underlying
+              factors contributing to your pain and modify your perception of
+              it. Through relaxation techniques, visualization, and suggestion,
+              we will work together to reduce pain intensity, promote healing,
+              and improve your overall well-being. Let's embark on this journey
+              towards a life with less pain and more comfort.
+            </p>
           )}
           {selectedTopic === "Overcome Fears" && (
-            <p>Information specific to the "Overcome Fears" topic goes here.</p>
+            <p>
+              Fears and phobias can significantly limit your life experiences
+              and hold you back from reaching your full potential. Hypnotherapy
+              can help you overcome these fears by reprogramming your
+              subconscious mind and replacing negative associations with
+              positive ones. Together, we will explore the root causes of your
+              fears and work towards releasing them. Through relaxation and
+              guided imagery, you will learn to respond differently to triggers,
+              gaining confidence and freedom. Let's take the courageous step
+              forward and conquer your fears, opening doors to new possibilities
+              and personal growth.
+            </p>
           )}
         </div>
       </div>
-      <div className="solution-tagline">Hypnosis is the solution you seek.</div>
       <Request />
     </div>
   );
