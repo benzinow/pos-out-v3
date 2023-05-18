758b8abc1da5d82b6a16e94feedd39d033bd5b58
benzinow <benzinow@gmail.com>
2023-05-18 15:05:18 -0700

    attempted to fix loader for mobile

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import Fixables from "./Fixables";
 import Bio from "./Bio";
@@ -10,9 +10,17 @@ import "./App.css";
 function App() {
   const [loading, setLoading] = useState(true);
 
-  window.onload = () => {
-    setLoading(false);
-  };
+  useEffect(() => {
+    const handleLoad = () => {
+      setLoading(false);
+    };
+
+    window.addEventListener("load", handleLoad);
+
+    return () => {
+      window.removeEventListener("load", handleLoad);
+    };
+  }, []);
 
   return (
     <div className="App">
