6fee0ca5d42782213e933be44200cc0dfa303906
benzinow <benzinow@gmail.com>
2023-05-18 14:51:00 -0700

    added loader to landing page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import Fixables from "./Fixables";
 import Bio from "./Bio";
 import Testimonies from "./Testimonies";
 import Footer from "./Footer";
+import Loader from "./Loader";
 import "./App.css";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true); // New state variable
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsLoading(false);
+    }, 500);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <div className="App">
-      <div className="body">
-        <Header />
-        <Fixables />
-        <Bio />
-        <Testimonies />
-        <Footer />
-      </div>
+      {isLoading ? ( // Conditionally render the loader while loading
+        <Loader />
+      ) : (
+        <div className="body">
+          <Header />
+          <Fixables />
+          <Bio />
+          <Testimonies />
+          <Footer />
+        </div>
+      )}
     </div>
   );
 }
