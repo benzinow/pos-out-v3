ddce46751b1f39bb364373c2ae683d142712507a
benzinow <benzinow@gmail.com>
2023-06-01 20:45:01 -0700

    certifications page updates

--- src/pages/Certifications.js
+++ src/pages/Certifications.js
@@ -1,14 +1,20 @@
 import React from "react";
 import Request from "./components/Request";
-import cert1 from "./images/cert01.png";
-import cert2 from "./images/cert02.png";
-import cert3 from "./images/cert03.png";
-import cert4 from "./images/cert04.jpg";
-import cert5 from "./images/cert05.png";
-import cert6 from "./images/cert06.png";
-import cert7 from "./images/cert07.jpg";
-import cert8 from "./images/cert08.png";
-import cert9 from "./images/cert09.png";
+import cert1 from "./images/cert001.png";
+import cert2 from "./images/cert002.jpg";
+import cert3 from "./images/cert003.png";
+import cert4 from "./images/cert004.jpg";
+import cert5 from "./images/cert005.png";
+import cert6 from "./images/cert006.png";
+import cert7 from "./images/cert007.png";
+import cert8 from "./images/cert008.png";
+import cert9 from "./images/cert009.jpg";
+import cert10 from "./images/cert010.png";
+import cert11 from "./images/cert011.png";
+import cert12 from "./images/cert012.png";
+import cert13 from "./images/cert013.jpg";
+import cert14 from "./images/cert014.png";
+import cert15 from "./images/cert015.png";
 
 export function Certifications() {
   return (
@@ -24,6 +30,12 @@ export function Certifications() {
         <img className="frame" src={cert7} alt="Topic Icon" />
         <img className="frame" src={cert8} alt="Topic Icon" />
         <img className="frame" src={cert9} alt="Topic Icon" />
+        <img className="frame" src={cert10} alt="Topic Icon" />
+        <img className="frame" src={cert11} alt="Topic Icon" />
+        <img className="frame" src={cert12} alt="Topic Icon" />
+        <img className="frame" src={cert13} alt="Topic Icon" />
+        <img className="frame" src={cert14} alt="Topic Icon" />
+        <img className="frame" src={cert15} alt="Topic Icon" />
       </div>
       <Request />
     </div>
